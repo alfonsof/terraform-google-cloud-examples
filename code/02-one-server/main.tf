fc3bb69e74663a72b1df71e55c27550dd6664d9d
Alfonso Fernandez-Barandiaran <alfonsofb@gmail.com>
2017-12-01 11:44:18 +0100

    Create main.tf

---
+++ code/02-one-server/main.tf
@@ -0,0 +1,27 @@
+provider "google" {
+  project     = "terraform-examples-gcloud"
+  # credentials = GOOGLE_CREDENTIALS
+  region = "us-east1"
+}
+
+resource "google_compute_instance" "example" {
+  name          = "example"
+  machine_type  = "f1-micro"
+  zone          = "us-east1-b"
+  
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-lts"
+    }
+  }
+  
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+  
+  tags = ["terraform-example"]
+}
