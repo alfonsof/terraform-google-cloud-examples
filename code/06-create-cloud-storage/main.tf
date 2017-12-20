102a3a8775b6b901338e2010fce869cc5c5d01d6
Alfonso Fernandez-Barandiaran <alfonsofb@gmail.com>
2017-12-20 12:10:54 +0100

    Create main.tf

---
+++ code/06-create-cloud-storage/main.tf
@@ -0,0 +1,9 @@
+provider "google" {
+  project     = "terraform-examples-gcloud"
+  # credentials = GOOGLE_CREDENTIALS
+  region = "us-east1"
+}
+
+resource "google_storage_bucket" "terraform_state" {
+  name     = "${var.bucket_name}"
+}
