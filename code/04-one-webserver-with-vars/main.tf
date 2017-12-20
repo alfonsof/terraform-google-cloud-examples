83e29b0037670ac2bb30a27eb8ab391e0418fe39
Alfonso Fernandez-Barandiaran <alfonsofb@gmail.com>
2017-12-20 11:34:56 +0100

    Update moving sections to vars.tf & outputs.tf

--- code/04-one-webserver-with-vars/main.tf
+++ code/04-one-webserver-with-vars/main.tf
@@ -1,8 +1,3 @@
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  default = "8080"
-}
-
 provider "google" {
   project     = "terraform-examples-gcloud"
   # credentials = GOOGLE_CREDENTIALS
@@ -43,8 +38,4 @@ resource "google_compute_instance" "example" {
   tags = ["terraform-example"]
   
   metadata_startup_script = "echo 'Hello, World' > index.html ; nohup busybox httpd -f -p ${var.server_port} &"
-}
-
-output "public_ip" {
-  value = "${google_compute_instance.example.network_interface.0.access_config.0.assigned_nat_ip}"
 }
